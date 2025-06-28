0223d3d02176773a5562895e3dc3b66598e9b854
AMSsharma <163770882+AMSsharma@users.noreply.github.com>
2025-06-28 21:42:18 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
     'backend.core',
 ]
 AUTHENTICATION_BACKENDS=[
