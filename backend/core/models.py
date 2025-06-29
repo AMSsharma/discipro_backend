6993e7d5a8c9dda98b37ed960ab96a30ac0edf95
AMSsharma <163770882+AMSsharma@users.noreply.github.com>
2025-06-29 18:03:43 +0530

    Update models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -20,7 +20,7 @@ class UserScore(models.Model):
     weekly_score = models.IntegerField(default=0)
     last_updated = models.DateTimeField(auto_now=True)
     last_score = models.IntegerField(default=0)  # to track score before the latest update
-    last_updated_date = models.DateField()
+    last_updated_date = models.DateField(default=datetime.date.today)
     def __str__(self):
         return f"{self.user.username} - {self.overall_score}"
 
